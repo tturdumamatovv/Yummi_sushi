fba6f76693c2f3ef30c24f62e8ff83293caaa538
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-07-15 17:31:02 +0600

    product by category slug

--- apps/product/api/views.py
+++ apps/product/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 from apps.product.api.serializers import ProductSerializer, SetSerializer
 from apps.product.models import Category, Product, Set
@@ -8,16 +9,23 @@ from apps.product.models import Category, Product, Set
 
 # Create your views here.
 class ProductListByCategorySlugView(generics.ListAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
+    def get(self, request, *args, **kwargs):
         slug = self.kwargs['slug']
         try:
             category = Category.objects.get(slug=slug)
         except Category.DoesNotExist:
             raise NotFound("Категория не найдена")
-        return Product.objects.filter(category=category)
 
+        products = Product.objects.filter(category=category)
+        sets = Set.objects.filter(category=category)
+
+        product_serializer = ProductSerializer(products, many=True)
+        set_serializer = SetSerializer(sets, many=True)
+
+        return Response({
+            'products': product_serializer.data,
+            'sets': set_serializer.data
+        })
 
 class SetListView(generics.ListAPIView):
     serializer_class = SetSerializer
