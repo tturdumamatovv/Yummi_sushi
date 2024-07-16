7151bc3320bbd7d2095826bfee35fab0c0abceab
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-07-16 15:19:33 +0600

    order message

--- apps/services/generate_message.py
+++ apps/services/generate_message.py
@@ -6,24 +6,28 @@ def generate_order_message(order, delivery_distance_km, delivery_fee):
         delivery_fee_info = ""
     else:
         delivery_info = (
-            f"Длина доставки: {delivery_distance_km:.2f} км\n"
+            f"Расстояние доставки: {delivery_distance_km:.2f} км\n"
             f"Стоимость доставки: {delivery_fee} сом\n"
         )
         delivery_fee_info = f"Стоимость доставки: {delivery_fee} сом\n"
 
     payment_info = (
         f"Способ оплаты: {order.get_payment_method_display()}\n"
-        f"Сдача с: {order.change if order.change else 0} сом\n"
+        f"Сумма наличными: {order.change if order.change else 0} сом\n"
+        f"Сдача: {order.change - order.total_amount if order.change else 0} сом\n"
     )
 
     message = (
         f"Новый заказ #{order.id}\n"
-        f"Пользователь: {order.user}\n"
+        f"Пользователь: {order.user.full_name if order.user.full_name else 'Имя не указано'}\n"
+        f"Номер: {order.user.phone_number}\n"
+
         f"Ресторан: {order.restaurant.name}\n"
 
-        "Детали заказа:\n"
+        "ЗАКАЗ:\n"
         "===============\n"
     )
+    message += "-----------------\n"
 
     for item in order.order_items.all():
         item_details = f"Продукт: {item.product_size.product.name} ({item.product_size.size.name})\n" if item.product_size else f"Сет: {item.set.name}\n"
@@ -41,12 +45,23 @@ def generate_order_message(order, delivery_distance_km, delivery_fee):
                 item_details += f" - {ingredient.name}\n"
 
         message += item_details + "-----------------\n"
-
+    message += "===============\n"
+    user_address = order.delivery.user_address
+    address = (
+        f"Город: {user_address.city}\n"
+        f"Адрес: {user_address.street} {user_address.house_number}\n"
+        f"Квартира: {user_address.apartment_number}\n"
+        f"Подъезд: {user_address.entrance}\n"
+        f"Этаж: {user_address.floor}\n"
+        f"Домофон: {user_address.intercom}\n"
+        f"Комментарии: {user_address.comment}\n"
+    )
     message += (
-        f"Адрес доставки: {order.delivery.user_address if not order.is_pickup else 'Самовывоз'}\n"
+        f"Адрес доставки:\n{address  if not order.is_pickup else 'Самовывоз'}\n"
         f"Статус: {order.get_order_status_display()}\n \n"
         f"{delivery_info}\n"
         f"{payment_info}\n"
+
         f"Общая сумма: {order.total_amount}\n"
 
     )
