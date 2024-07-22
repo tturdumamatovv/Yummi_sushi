2a5bbce19f13c1b55ae2f9f4149ef9b770ecf654
tturdumamatovv <muki2022@mail.ru>
2024-07-22 14:29:43 +0600

    a

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static'),]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
