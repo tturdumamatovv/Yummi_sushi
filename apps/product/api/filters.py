5b434a186ff4da2e37e56e3d89a237fa00e7b4d2
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-07-25 10:49:43 +0600

    search fix

--- apps/product/api/filters.py
+++ apps/product/api/filters.py
@@ -4,8 +4,8 @@ from apps.product.models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    title = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Product
-        fields = ['title']
+        fields = ['name']
