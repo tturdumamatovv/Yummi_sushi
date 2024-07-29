388e162be80c4141b98108239c1bd853ba383937
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-07-29 16:47:29 +0600

    app download link

--- apps/authentication/signals.py
+++ apps/authentication/signals.py
@@ -7,9 +7,9 @@ from ..services.get_coordinates import get_coordinates
 
 @receiver(pre_save, sender=UserAddress)
 def set_coordinates(sender, instance, **kwargs):
-    if instance.city or instance.street or instance.house_number and (
+    if instance.city and (
             instance.latitude is None or instance.longitude is None):
-        address = f"{instance.city} {instance.street} {instance.house_number}"
+        address = f"{instance.city}"
         latitude, longitude = get_coordinates(address)
         if latitude and longitude:
             instance.latitude = latitude
