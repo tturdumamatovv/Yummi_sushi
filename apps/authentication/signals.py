70622c357040310caf0efcb2e3a7593774e9175b
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-07-30 11:33:12 +0600

    app download link

--- apps/authentication/signals.py
+++ apps/authentication/signals.py
@@ -1,19 +1,19 @@
-# from django.db.models.signals import pre_save
-# from django.dispatch import receiver
-#
-# from .models import UserAddress
-# from apps.orders.models import TelegramBotToken
-# from ..services.get_coordinates import get_coordinates
-# token = TelegramBotToken.objects.first()
-#
-#
-# @receiver(pre_save, sender=UserAddress)
-# def set_coordinates(sender, instance, **kwargs):
-#     if instance.city and (
-#             instance.latitude is None or instance.longitude is None):
-#         address = f"{instance.city}"
-#         latitude, longitude = get_coordinates(address, token.google_map_api_key)
-#         if latitude and longitude:
-#             instance.latitude = latitude
-#             instance.longitude = longitude
-#
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from .models import UserAddress
+from apps.orders.models import TelegramBotToken
+from apps.services.get_coordinates import get_coordinates
+token = TelegramBotToken.objects.first()
+
+
+@receiver(pre_save, sender=UserAddress)
+def set_coordinates(sender, instance, **kwargs):
+    if instance.city and (
+            instance.latitude is None or instance.longitude is None):
+        address = f"{instance.city}"
+        latitude, longitude = get_coordinates(address, token.google_map_api_key)
+        if latitude and longitude:
+            instance.latitude = latitude
+            instance.longitude = longitude
+
