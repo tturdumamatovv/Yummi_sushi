da5d5a4cbc3c6ad86d25e834fdd0917313aa38e7
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-09-16 12:27:45 +0600

    chat base

--- config/asgi.py
+++ config/asgi.py
@@ -1,29 +1,33 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
+
 import django
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()  # Ensure Django is fully setup before importing channels and apps
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from apps.orders.routing import ws_urlpatterns
 from apps.support_admin_chat.routing import websocket_urlpatterns
 
+import os
+import django
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from .middleware import SimpleCorsMiddleware  # Ensure you import the correct path
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+django.setup()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            ws_urlpatterns + websocket_urlpatterns,
+    "websocket": AllowedHostsOriginValidator(
+        SimpleCorsMiddleware(  # Use your custom middleware here
+            AuthMiddlewareStack(
+                URLRouter(
+                    ws_urlpatterns + websocket_urlpatterns,
+                )
+            )
         )
     ),
 })
--- config/middleware.py
+++ config/middleware.py
@@ -1,4 +1,6 @@
 from django.utils import translation
+from channels.middleware import BaseMiddleware
+from urllib.parse import urlparse
 
 
 class LanguageMiddleware:
@@ -15,3 +17,25 @@ class LanguageMiddleware:
         translation.deactivate()
 
         return response
+
+
+
+class SimpleCorsMiddleware(BaseMiddleware):
+    def __init__(self, inner):
+        super().__init__(inner)
+
+    async def __call__(self, scope, receive, send):
+        # Continue with the connection only if it's a WebSocket type
+        if scope['type'] == 'websocket':
+            headers = dict(scope['headers'])
+
+            # If 'origin' header is present, manage CORS for WebSocket
+            if b'origin' in headers:
+                origin = headers[b'origin'].decode('utf-8')
+                parsed_url = urlparse(origin)
+                # Modify the scope to include the CORS headers
+                scope['headers'].extend([
+                    (b'access-control-allow-origin', bytes(origin, 'utf-8')),
+                    (b'access-control-allow-credentials', b'true')
+                ])
+        return await super().__call__(scope, receive, send)
