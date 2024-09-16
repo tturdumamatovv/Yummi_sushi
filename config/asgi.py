1d7ddc141343151bf3a219f604aed8537a9d5d35
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-09-16 15:05:26 +0600

    chat base

--- config/asgi.py
+++ config/asgi.py
@@ -8,16 +8,11 @@ django.setup()  # Ensure Django is fully setup before importing channels and app
 from apps.orders.routing import ws_urlpatterns
 from apps.support_admin_chat.routing import websocket_urlpatterns
 
-import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from .middleware import SimpleCorsMiddleware  # Ensure you import the correct path
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
