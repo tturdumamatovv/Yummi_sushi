94c47a61fc5515545a9b0b5e4b9b38d3bac14b1c
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-09-16 10:58:18 +0600

    chat base

--- apps/support_admin_chat/models.py
+++ apps/support_admin_chat/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.conf import settings
 
+from apps.authentication.models import User
+
 
 class ChatRoom(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='chat_room', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='chat_room', on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"ChatRoom for {self.user.username} (Active: {self.active})"
+        return f"ChatRoom for {self.user.phone_number} (Active: {self.active})"
 
 
 class ChatMessage(models.Model):
     room = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE)
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
