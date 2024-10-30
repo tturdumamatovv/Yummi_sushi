7e75a347f7084fb2b80134fa931be016ead61b5c
Nurmuhammed <muki2022@mail.ru>
2024-10-30 17:05:43 +0600

    added fields in OrderListSerializers

--- apps/orders/api/serializers.py
+++ apps/orders/api/serializers.py
@@ -107,7 +107,7 @@ class OrderListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'total_amount', 'order_time', 'restaurant', 'order_items', 'total_bonus_amount',
-                  'is_pickup', 'user_address', 'app_download_url', 'order_status']
+                  'is_pickup', 'user_address', 'app_download_url', 'order_status', 'payment_method', 'order_source']
 
     def get_total_amount(self, obj):
         return obj.get_total_amount_2()
